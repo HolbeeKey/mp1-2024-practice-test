1f0b39eff60c2200d67f4818a152c75f5c393567
HolbeeKey <156485689+HolbeeKey@users.noreply.github.com>
2024-12-19 10:29:03 +0300

    Update task1.c

--- 3824Б1ПР3/07 Климаков Никита Николаевич/task1.c
+++ 3824Б1ПР3/07 Климаков Никита Николаевич/task1.c
@@ -1,7 +1,32 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел, среди которых
 // не менее двух нулей. Найти сумму элементов, расположенных между первыми
 // двумя нулями.
+#include <stddef.h>
+
 int task1(int A[], size_t n)
 {
-  return -1;
+    size_t first_zero_index = -1;
+    size_t second_zero_index = -1;
+    
+    
+    for (size_t i = 0; i < n; ++i) {
+        if (A[i] == 0) {
+            if (first_zero_index == -1) {
+                first_zero_index = i;
+            } else if (second_zero_index == -1) {
+                second_zero_index = i;
+                break; /
+            }
+        }
+    }
+
+    
+    int sum = 0;
+    if (first_zero_index != -1 && second_zero_index != -1 && second_zero_index > first_zero_index + 1) {
+        for (size_t i = first_zero_index + 1; i < second_zero_index; ++i) {
+            sum += A[i];
+        }
+    }
+
+    return sum;
 }
