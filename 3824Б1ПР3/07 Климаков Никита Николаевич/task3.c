140b7af8d042795d2d842490c5572d483a71e30a
HolbeeKey <156485689+HolbeeKey@users.noreply.github.com>
2024-12-19 10:30:50 +0300

    Update task3.c

--- 3824Б1ПР3/07 Климаков Никита Николаевич/task3.c
+++ 3824Б1ПР3/07 Климаков Никита Николаевич/task3.c
@@ -1,7 +1,36 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
 // заглавных букв английского алфавита, содержащихся в исходной строке s. 
 // Пример: s = "aBc1D2e34F56g", ответ: "BDF".
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
 char* task3(char *s)
 {
-  return "task3";
+    
+    size_t count = 0;
+    for (size_t i = 0; s[i] != '\0'; ++i) {
+        if (isupper(s[i])) {
+            count++;
+        }
+    }
+
+    
+    char *result = (char*)malloc(count + 1);
+    if (result == NULL) {
+        return NULL; 
+
+    
+    size_t j = 0;
+    for (size_t i = 0; s[i] != '\0'; ++i) {
+        if (isupper(s[i])) {
+            result[j++] = s[i];
+        }
+    }
+
+   
+    result[j] = '\0';
+
+    return result;
 }
